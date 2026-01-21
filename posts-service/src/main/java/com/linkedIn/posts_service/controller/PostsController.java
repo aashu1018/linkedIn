6c3a197440578aedf96b58de6d65a861ef0022c6
Ashutosh <ashu.ksing1018@gmail.com>
2026-01-21 12:26:05 +0530

    entity, dto and repository

---
+++ posts-service/src/main/java/com/linkedIn/posts_service/controller/PostsController.java
@@ -0,0 +1,11 @@
+package com.linkedIn.posts_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class PostsController {
+}
