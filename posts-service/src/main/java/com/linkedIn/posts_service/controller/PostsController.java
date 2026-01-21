f4ff1392b22f5c26ae4073243f0fa22d8eec8521
Ashutosh <ashu.ksing1018@gmail.com>
2026-01-21 12:54:09 +0530

    service and controller

--- posts-service/src/main/java/com/linkedIn/posts_service/controller/PostsController.java
+++ posts-service/src/main/java/com/linkedIn/posts_service/controller/PostsController.java
@@ -1,6 +1,14 @@
 package com.linkedIn.posts_service.controller;
 
+import com.linkedIn.posts_service.dto.PostCreateRequestDTO;
+import com.linkedIn.posts_service.dto.PostDTO;
+import com.linkedIn.posts_service.service.PostsService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostsController {
+
+    private final PostsService postsService;
+
+    @PostMapping
+    private ResponseEntity<PostDTO> createPost(@RequestBody PostCreateRequestDTO postDTO, HttpServletRequest httpServletRequest){
+        PostDTO createdPost = postsService.createPost(postDTO, 1L);
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
 }
---
+++ posts-service/src/main/java/com/linkedIn/posts_service/service/PostsService.java
@@ -0,0 +1,28 @@
+package com.linkedIn.posts_service.service;
+
+import com.linkedIn.posts_service.dto.PostCreateRequestDTO;
+import com.linkedIn.posts_service.dto.PostDTO;
+import com.linkedIn.posts_service.entity.Post;
+import com.linkedIn.posts_service.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostsService {
+
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+
+    public PostDTO createPost(PostCreateRequestDTO postDTO, Long userId) {
+        Post post = modelMapper.map(postDTO, Post.class);
+        post.setUserId(userId);
+
+        Post savedPost = postRepository.save(post);
+
+        return modelMapper.map(savedPost, PostDTO.class);
+    }
+}
