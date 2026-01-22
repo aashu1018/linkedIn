046837df2b6e2448f5b8d55c8bd5b677cc42487b
Ashutosh <ashu.ksing1018@gmail.com>
2026-01-22 21:02:37 +0530

    exception handling

---
+++ posts-service/src/main/java/com/linkedIn/posts_service/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.linkedIn.posts_service.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message){
+        super(message);
+    }
+}
---
+++ posts-service/src/main/java/com/linkedIn/posts_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.linkedIn.posts_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<APIError> handleBadRequestException(BadRequestException exception){
+        APIError error = new APIError(exception.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<APIError> handleResourceNotFoundException(ResourceNotFoundException exception){
+        APIError error = new APIError(exception.getLocalizedMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+}
