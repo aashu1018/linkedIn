85f707c66729cfa1e6161071a6dc721c17c8f1b9
Ashutosh <ashu.ksing1018@gmail.com>
2026-01-29 02:38:14 +0530

    connections service

--- connections-service/src/main/java/com/linkedIn/connections_service/controller/ConnectionsController.java
+++ connections-service/src/main/java/com/linkedIn/connections_service/controller/ConnectionsController.java
@@ -1,7 +1,22 @@
 package com.linkedIn.connections_service.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.linkedIn.connections_service.entity.Person;
+import com.linkedIn.connections_service.service.ConnectionsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/core")
+@RequiredArgsConstructor
 public class ConnectionsController {
+
+    private final ConnectionsService connectionsService;
+
+    @GetMapping("/{userId}/first-degree")
+    private ResponseEntity<List<Person>> getFirstConnections(@PathVariable Long userId){
+        return ResponseEntity.ok(connectionsService.getFirstDegreeConnections(userId));
+    }
 }
